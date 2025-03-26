90da82639bc43cceb055b467e3efe77f6e02697e
WilliamDavid2 <83736535+WilliamDavid2@users.noreply.github.com>
2025-03-26 10:23:02 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+//Variables de entorno
+dotenv.config();
+const port = process.env.PORT || 4000;
+
+//WebSockets
+io.on('connection', (ws) => {
+    console.log('Connection established');
+    ws.on('new message', (msg) => {
+        console.log(`${msg}`);
+        ws.join("some room");
+        ws.to("some room").emit('new message', `${msg}`);
+        //ws.emit('new message', `${msg}`);
+
+    })
+})
+
+//Rutas
+/*app.get('/', (req, res) => {
+    res.send('Server running!');
+})*/
+
+server.listen(3000, () => {
+    console.log(`Server running on port ${port}`);
+});
